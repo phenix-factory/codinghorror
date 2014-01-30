cd2df7ec1ad00a3d2990bc1201114507b6d41768
Debondt Didier <p@henix.be>
2014-01-30 20:11:13 +0100

    On supprime le couteau suisse et on utilise le plugin bloc dépliable a la place.

---
+++ plugins/auto/blocsdepliables/v1.1.1/lang/blocsdepliables_en.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/blocsdepliables?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'blocsdepliables_titre' => 'Expanding containers blocks',
+
+	// C
+	'cfg_exemple' => 'Example',
+	'cfg_exemple_explication' => 'Example explanation',
+	'cfg_titre_parametrages' => 'Parameters',
+
+	// E
+	'explication_cookie' => 'The state of the numbered blocks will be stored in a cookie during the session, in order to keep the page’s aspect for a subsequent visit.',
+	'explication_unique' => 'Opening a block will trigger all the other blocks on the page to close, in order to have one single opened block at the time.',
+
+	// L
+	'label_animation' => 'Animation',
+	'label_animation_aucun' => 'None',
+	'label_animation_fast' => 'Fast slide',
+	'label_animation_millisec' => 'Slide duration :',
+	'label_animation_normal' => 'Normal slide',
+	'label_animation_slow' => 'Slow slide',
+	'label_balise_titre' => 'Tag for the blocks title',
+	'label_cookie_1' => 'Remember the blocks state',
+	'label_unique_1' => 'A single block opened on the page',
+
+	// T
+	'titre_page_configurer_blocsdepliables' => 'Expanding containers blocks'
+);
+
+?>
